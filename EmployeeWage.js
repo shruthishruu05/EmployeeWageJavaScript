0ebdf57e2d48fa48b90e098d659ff3176fc980a0
shruthishruu05 <shruthi.m@perfios.com>
2021-10-05 16:17:36 +0530

    REFACTORED : ability to calculate wage for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,8 +4,9 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
-//uc3
+
 function getWorkingHours(empCheck) {
     switch(empCheck)
     {
@@ -18,8 +19,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empHrs = 0;
+let totalEmpHrs = 0;
+for(let day =0;day< NUM_OF_WORKING_DAYS;day++)
+{
 let empCheck = Math.floor(Math.random()*10)%3;
 empHrs = getWorkingHours(empCheck);
+}
 let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp Wage: "+ empWage);
+console.log("Employee hours is : "+totalEmpHrs+ " Emp Wage: "+ empWage);
