292f1ac5ad5032a6b2cf99dd49467b76eab0f28f
shruthishruu05 <shruthi.m@perfios.com>
2021-10-05 16:11:58 +0530

    REFACTORED : removed comment

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,12 +6,10 @@ const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 
 //uc3
-function getWorkingHours(empCheck) 
-{
+function getWorkingHours(empCheck) {
     switch(empCheck)
     {
-        
-        case IS_PART_TIME:
+       case IS_PART_TIME:
             return PART_TIME_HOUR;
         case IS_FULL_TIME:
             return FULL_TIME_HOUR;
@@ -19,6 +17,7 @@ function getWorkingHours(empCheck)
             return 0;
     }
 }
+
 let empHrs = 0;
 let empCheck = Math.floor(Math.random()*10)%3;
 empHrs = getWorkingHours(empCheck);
