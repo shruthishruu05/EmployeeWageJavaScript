af6bf836da3d8c312860472ad39c82d8ce9f3fa2
shruthishruu05 <shruthi.m@perfios.com>
2021-10-06 19:43:48 +0530

    REFACTORED : storing daily wage in a map

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -30,13 +30,16 @@ function calcDailyWage(empHrs) {
 let totalEmpHrs =0;
 let totalWorkingDays =0;
 let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
 while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
 }
+
 let empWage = calcDailyWage(totalEmpHrs);
 console.log("EMPLOYEE HOURS IS : "+totalEmpHrs+" hr\nAND TOTAL WORKING DAYS : " + totalWorkingDays + "\nAND EMPLOYEE WAGE IS : " + empWage)
 
@@ -99,3 +102,10 @@ function totalDaysWorked(numOfDays,dailyWage) {
     return numOfDays;
 }
 console.log("UC 7G- Number od days Emp Worked "+ empDailyWageArr.reduce(totalDaysWorked,0));
+
+console.log(empDailyWageMap);
+
+function totalWage(totalWage,dailyWage) {
+    return totalWage+dailyWage;
+}
+console.log("UC7A -Emp Wage Map totalHrs: " + Array.from(empDailyWageMap.values()).reduce(totalWages,0));
